87f566b799e3a90f99adf85ece9392ce22ead80b
unknown <anish1204>
2024-02-03 01:35:35 +0530

    documentation added

--- Readme.md
+++ Readme.md
@@ -1,67 +1,28 @@
-# Coding Assessment L-2 Code Submission Process with Boilerplate
+# Coding Assignment
 
-Welcome to the coding assessment l2. We are excited to see your skills in action. Please follow the instructions below to submit your solution.
+# Live Project Link 
+```
+https://anish1204.github.io/Shoppy_Website/
+```
 
-## Getting Started
+## Requirements Completed
+1. Product Vendor 
+2. Product title 
+3. Price 
+4. Compare at price
+5. Percentage off
+6. Product Image
+7. Thumbnails
+8. Color Selector
+9. Size selector
+10. Quantity selector
+11. Add to cart buttton,
 
-1. **Clone or Download:**
-   - If using Git, clone the repository using the following command:
-     ```
-     git clone https://github.com/mmdmrishabh/Coding-assessment-l2-Boilerplate-code.git
-     ```
-   - Alternatively, download the code as a ZIP file and extract it.
 
-2. **Navigate to Project Directory:**
-
-3. **Explore the Structure:**
-Take a moment to review the project structure:
-
-        project-root/
-        |-- index.html
-        |-- style.css
-        |-- script.js
-        |-- README.md
-- `index.html`: Main HTML file for your project.
-- `style.css`: CSS file for styling.
-- `script.js`: JavaScript file for scripting logic.
-- `README.md`: Documentation for your project.
-  
-> **Important:** Avoid using additional files or a single file for your code. Feel free to organize asset files by creating a separate folder.
 
 ## Development and Testing
 
-4. **Start Working:**
-- Read all requirements that are shared with design files.
-- Open `index.html` in your browser to view the initial state of the project, And start coding.
-- Modify the `style.css` and `script.js` files to implement your solution.
-5. **Testing:**
-- Test your code to ensure it produces the expected output.
-- Ensure that the console (Developer Tools) does not display any errors.
+Clone the project and run the html file
 
+Feel free to reach out to at `anishpatil304@gmail.com` for further Contact
 
----
-## Submission Guidelines
-
-### Option 1: GitHub Repository
-
-1. Create a new public repository on GitHub.
-2. Upload your project files (index.html, style.css, script.js) to the repository.
-3. Write a brief description of your project in the README.md file.
-4. Share the repository URL with us.
-
-### Option 2: ZIP File on Google Drive
-
-1. Create a ZIP file of your entire project.
-2. Upload the ZIP file to your Google Drive.
-3. Share the public link to the ZIP file via email.
-
-## Hosting (Bonus)
-You can earn bonus points by hosting your project on a free hosting platform such as GitHub Pages or Netlify.
-
-## Important Notes
-
-- Do not use any third-party libraries or frameworks.
-- Ensure that your project works as intended and is free from errors.
-- Make sure to include clear instructions on how to run your project (if necessary).
-
-Feel free to reach out to invigilator at `rishabh@marmeto.com` if you have any questions. Good luck! We look forward to reviewing your submission.
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// console.log('====================================');
-// console.log("Connected");
-// console.log('====================================');
+
+// Variables declared for Further use 
+let count = 1;
+let ProductTitle;
+let SelectedColor;
+let SelectedSize;
 
 const dataOfForm = document.getElementById('radioContainer');
+
+
 const dataOfColor = document.getElementById('buttonContainer');
 
-
-
+// On any Changes in radio inputs of Size the variable will store the latest selected size 
 dataOfForm.addEventListener('change',function(event){
     if(event.target.type === 'radio')
     {
@@ -14,6 +18,10 @@ dataOfForm.addEventListener('change',function(event){
         SelectedSize=event.target.value;
     }
 })
+
+
+
+// Latest selected color after clicking the Options will be selected
 dataOfColor.addEventListener('click',function(event){
     if(event.target.type === 'button')
     {
@@ -23,6 +31,9 @@ dataOfColor.addEventListener('click',function(event){
 })
 
 
+
+
+// Images  previewing Fuction
 function previewImage(imageSrc) {
     var previewImage = document.getElementById('previewImage');
     previewImage.src = imageSrc;
@@ -30,11 +41,9 @@ function previewImage(imageSrc) {
 
 
 
-let count = 1;
-let ProductTitle;
-let SelectedColor;
-let SelectedSize;
 
+
+// Count Update Code
 function updateCount() {
     document.getElementById('count').textContent = count;
 }
@@ -49,34 +58,38 @@ document.getElementById('decrementButton').addEventListener('click', function ()
     }
 });
 
+
+
+
+
+// After Submitting the text to be displayed is shown in  below format
+
 function showRadioValue()
 {
-    // console.log('Hi ');
     let text=ProductTitle+' with Color '+SelectedColor+ ' and Size '+ SelectedSize +' added to cart';
     document.getElementById('Onsubmittext').innerText =text;
 }
 
+//Fetching API 
 fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json?v=1701948448').then(res => res.json()).then(data => {
 
 
     const buttonData = data.product.options[0].values;
-    // console.log(buttonData[0] + ' Hi');
-
     
     
     let i=0;
     let colors =[];
     const buttonContainer = document.getElementById('buttonContainer');
+    //targeting the button container to insert Buttons into it according to the JSON
     buttonData.forEach(buttonData => {
         const button = document.createElement('button');
         let color = buttonData[1];
         for (let key in buttonData) {
             colors.push(key);
-            // console.log(key + ': ' + buttonData[key]);
+            
         }
         button.style.backgroundColor = colors[i];
         button.type='button';
-        button.onclick="alert('Button 2 Clicked')";
         button.style.width = '50px';
         button.style.height = '50px';
         button.style.marginLeft = '0.5rem';
@@ -87,11 +100,12 @@ fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json
         i++;
     })
 
+
+     //Similar as above Same is done for Radio 
     const sizeData = data.product.options[1].values;
 
     const radioContainer = document.getElementById('radioContainer');
-    // console.log(sizeData);
-
+    
     sizeData.forEach(sizeData => {
         const radioLabel = document.createElement('label');
         const radioInput = document.createElement('input');
@@ -104,7 +118,7 @@ fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json
         radioContainer.appendChild(radioLabel);
     })
 
-
+    // Updating the Required imformation like org_price , discounted_price below
     ProductTitle= data.product.title;
     document.getElementById('Pdttitle').innerText = data.product.vendor;
     document.getElementById('DisPrice').innerText = data.product.price;
@@ -114,15 +128,16 @@ fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json
     
     let final_price = data.product.price;
     final_price = final_price.substring(1,final_price.length);
-    // console.log(final_price);
+    
+
+
+    //Calculating the discount and updating the values accordingly
     let initial_price = data.product.compare_at_price;
     initial_price = initial_price.substring(1,initial_price.length);
     let discount = initial_price-final_price;
     discount = discount/initial_price;
     discount=discount*100;
     // console.log(discount|0);
-
-
     text = text.substring(25, text.length - 4);
     document.getElementById('DiscountedAmount').innerText = (discount|0) + '% Off';
     document.getElementById('description').innerText = text;
